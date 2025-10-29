3bc0d6be76a678d3aab756537e1574e672910dc0
abnercoolman <abnercoolman@gmail.com>
2025-10-29 11:14:11 -0300

    feat(auth): Adiciona middleware de token e endpoint de verificação de usuário
    - Implementa middleware Gettoken para extrair o JWT dos cabeçalhos
    - Adiciona endpoint '/checkUser' para verificar o status de usuário autenticado
    - Atualiza rotas de usuário para incluir o novo endpoint de verificação

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 // routes/userRoutes.js
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import {
+    createUser,
+    loginUser,
+    checkUser,
+} from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/user", createUser);
 userRoutes.post("/auth", loginUser);
+userRoutes.get("/checkUser", checkUser);
 
 export default userRoutes;

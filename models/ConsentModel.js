0b64d3c0e0b3eb77cbb2cff9427cac26271750fb
Leticia Rocha <moraesleticia680@gmail.com>
2025-11-04 08:57:31 -0300

    feat: adição do ConsentModel.js

---
+++ models/ConsentModel.js
@@ -0,0 +1,46 @@
+// models/Consent.js
+import mongoose from 'mongoose';
+
+// clientAppId -> é necessário nesse sistema??
+// permissions -> é necessário nesse sistema??
+
+const ConsentSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            required: [true, 'O ID é obrigatório'],
+        },
+        customerId: {
+            type: String,
+            required: [true, 'O ID do cliente é obrigatório'],
+        },
+        clientAppId: {
+            type: String,
+            required: [true, 'O clientAppId é obrigatório'],
+        },
+        permissions: {
+            type: [String],
+            required: [true, 'As permissões são obrigatórias'],
+        },
+        status: {
+            type: String,
+            enum: ['active', 'revoked'],
+            required: [true, 'O status é obrigatório'],
+        },
+        createdAt: {
+            type: Date,
+            required: [true, 'A data de criação do consent é obrigatória'],
+        },
+        expiresAt: {
+            type: Date,
+            required: [true, 'A data de expiração do consent é obrigatória'],
+        }
+
+    },
+    {
+        collection: 'consents',
+    }
+)
+
+const Consent = mongoose.model('Consent', ConsentSchema);
+export default Consent;
